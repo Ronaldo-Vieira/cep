e54d2ef3c22615e99620539c31087a37f7e216a8
Ronaldo Vieira <88357501+Ronaldo-Vieira-Lira@users.noreply.github.com>
2021-11-22 04:21:23 -0300

    cep

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from 'react';
+import api from "./api"
+import './App.css';
+
+function App() {
+  const [cepi, setCep] = useState()
+  const [dados, setDados] = useState();
+  useEffect(() => {
+    api.get(`${cepi}/json`).then((response) => {
+      setDados(response.data)
+    })
+  })
+  function pegaCep(passa) {
+    setCep(passa)
+  }
+  return (
+    <div className="App">
+      <h1>Buscador de cep</h1>
+      <br />
+      <input type="text" id="clientCep" />
+      <button onClick={() => {
+        pegaCep(document.getElementById("clientCep").value)
+      }}> Enviar </button>
+      <br />
+      <div id="container">
+
+        <div className="cssCep">
+          Logradouro: {dados?.logradouro}
+          <br />
+          Cep: {dados?.cep}
+          <br />
+          Bairro: {dados?.bairro}
+
+        </div>
+        <div className="cssCep">
+          Cidade: {dados?.localidade}
+          <br />
+          UF: {dados?.cud}
+          <br />
+          Código DDD: {dados?.ddd}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
